c62cb8298fe1ea509c426151dbaf12ee46d2080e
mumud1 <155530032+mumud1@users.noreply.github.com>
2024-07-08 15:20:23 +0800

    reverted to previous colors

--- game.js
+++ game.js
@@ -243,14 +243,14 @@ fetch(`https://${hostname}:${port}/games`, {
                             // ctx.fillStyle = 'rgb(211, 211, 211)'; // lighter shade of gray
                             break;
                         case elements.unknown:
-                            ctx.fillStyle = 'rgb(211, 211, 211)'; //'rgb(64, 64, 64)'; // very dark gray
+                            ctx.fillStyle = 'grey'; //'rgb(64, 64, 64)'; // very dark gray
                             ctx.fillRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             ctx.strokeStyle = 'white'; // set border color to white
                             ctx.lineWidth = 1; // set border width
                             ctx.strokeRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             break;
                         case elements.traversable:
-                            ctx.fillStyle = 'rbg(172, 225, 175)'; //'rgb(105, 105, 105)'; // dark gray
+                            ctx.fillStyle = 'green'; //'rgb(105, 105, 105)'; // dark gray
                             ctx.fillRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             ctx.strokeStyle = 'white'; // set border color to white
                             ctx.lineWidth = 1; // set border width
