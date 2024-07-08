0775cd743d44effd79c53b3389d83522e683363f
mumud1 <155530032+mumud1@users.noreply.github.com>
2024-07-08 14:50:32 +0800

    changed the color scheme of unknown and known paths

--- game.js
+++ game.js
@@ -243,14 +243,14 @@ fetch(`https://${hostname}:${port}/games`, {
                             // ctx.fillStyle = 'rgb(211, 211, 211)'; // lighter shade of gray
                             break;
                         case elements.unknown:
-                            ctx.fillStyle = 'black'; //'rgb(64, 64, 64)'; // very dark gray
+                            ctx.fillStyle = 'rgb(211, 211, 211)'; //'rgb(64, 64, 64)'; // very dark gray
                             ctx.fillRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             ctx.strokeStyle = 'white'; // set border color to white
                             ctx.lineWidth = 1; // set border width
                             ctx.strokeRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             break;
                         case elements.traversable:
-                            ctx.fillStyle = 'green'; //'rgb(105, 105, 105)'; // dark gray
+                            ctx.fillStyle = 'rbg(172, 225, 175)'; //'rgb(105, 105, 105)'; // dark gray
                             ctx.fillRect(col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE);
                             ctx.strokeStyle = 'white'; // set border color to white
                             ctx.lineWidth = 1; // set border width
@@ -405,7 +405,7 @@ fetch(`https://${hostname}:${port}/games`, {
             winnerDiv.style.zIndex = '1000';
             winnerDiv.innerHTML = `<h1>Player ${playerId} Won!</h1>`;
         
-            const closeButton = document.createElement('button'); // NEW: Close button
+            const closeButton = document.createElement('button');
             closeButton.innerText = 'X';
             closeButton.style.position = 'absolute';
             closeButton.style.top = '-1px';
@@ -421,7 +421,7 @@ fetch(`https://${hostname}:${port}/games`, {
                 nextGame();
             });
         
-            winnerDiv.appendChild(closeButton); // NEW: Append close button
+            winnerDiv.appendChild(closeButton); 
             winnerDiv.appendChild(nextGameButton);
             document.body.appendChild(winnerDiv);
         }
