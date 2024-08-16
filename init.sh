fc083b420660d53385230f6941efa050a7f341b7
danielh13210 <danielh13210@gmail.com>
2024-08-16 16:44:21 +0700

    Update launcher
    Wait for frontend server to start before opening browser

--- init.sh
+++ init.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 ./webserver/abyssws &
-XAPP_FORCE_GTKWINDOW_ICON="$PWD/favicon.ico" firefox --kiosk --class="miningbots" localhost:8000
+TIME=0
+STARTED=false
+while ! $STARTED; do
+    echo -en "\rWaiting for frontend server to start... ($TIME seconds elapsed)"
+    pidof abyssws-x64 && STARTED=true || pidof abyssws-x86 && STARTED=true
+    sleep 1
+    ((TIME++))
+    if [ $TIME -gt 20 ]; then
+        echo -e "\rFailed to start frontend server.                            "
+        exit
+    fi
+done
+echo
+source browsersettings.conf
+$ENABLE_KIOSK_MODE && KIOSK="--kiosk" || KIOSK=""
+$TEST_MODE && ../miningbots/build/bin/miningbots &
+XAPP_FORCE_GTKWINDOW_ICON="$PWD/favicon.ico" firefox $KIOSK --class="Mining Bots" localhost:8000
+pkill -2 abyssws
