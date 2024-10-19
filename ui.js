dfa784fcd68b7490c2e4b685156ef38d856d7991
danielh13210 <danielh13210@gmail.com>
2024-10-19 12:52:50 +0800

    Add help icon
    Add a help icon at the top right corner next to the Settings button

---
+++ ui.js
@@ -0,0 +1,9 @@
+function show_help(){
+    alert(
+        `Keyboard shortcuts:
+            
+        Control-Alt-D: Show/Hide Settings button
+        Control-Alt-C: Open Settings panel
+        Control-H:     Open this help`
+    );
+}
