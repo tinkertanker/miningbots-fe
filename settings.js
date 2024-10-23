bd1aee83d4d16965a962f6ad39f808ae4948eafe
danielh13210 <danielh13210@gmail.com>
2024-10-23 16:47:07 +0800

    Update settings.js
    Tidy up code by removing duplicated code into functions

--- settings.js
+++ settings.js
@@ -75,16 +75,24 @@ function attachBeforeUnload(){
     });
 }
 
+function read_settings_button_visibility_cookie(){
+    let displayed=getCookie("settings_button_displayed");
+    if(!displayed)return true;//if cookie value is falsy (cookie cannot be read), assume value is true
+    return displayed=="true";
+}
+
 function update_settings_button_visibility(){
-    let displayed=getCookie("settings_button_displayed")=="true";
+    set_settings_button_visibility(read_settings_button_visibility_cookie());
+}
+
+function set_settings_button_visibility(displayed){
     document.querySelector("#settings-button").style.visibility=displayed?"visible":"collapse";
 }
 
 function toggle_settings_button(){
     //invert the boolean in the settings_button_displayed cookie
-    let displayed=getCookie("settings_button_displayed")=="true";
+    let displayed=read_settings_button_visibility_cookie();
     let new_displayed=!displayed;
     setCookie("settings_button_displayed",new_displayed.toString(),"Fri, 31 Dec 9999 23:59:59 GMT");
-
-    document.querySelector("#settings-button").style.visibility=new_displayed?"visible":"collapse";
+    set_settings_button_visibility(new_displayed);
 }
