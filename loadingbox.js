a57a2b367fd7df42be659df1c4078fd0fe6c3539
danielh13210 <danielh13210@gmail.com>
2025-01-01 13:14:49 +0800

    Use getElementById
    Replace querySelector for Loading Box

--- loadingbox.js
+++ loadingbox.js
@@ -5,7 +5,7 @@ const LB_SERVER_UNAVAILABLE = 2;
 const LB_NO_INTERNET = 3;
 let LB_OBJECT = null;
 function initializeLoadingBox() {
-    LB_OBJECT = document.querySelector(".loadingbox");
+    LB_OBJECT = document.getElementById("loadingbox");
 }
 function setLoadingBoxStatus(status) {
     switch (status) {
