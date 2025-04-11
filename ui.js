b60060109a068688d3383133528d714c31f73534
danielh13210 <danielh13210@gmail.com>
2025-04-11 15:52:39 +0800

    Remove the Control-Alt-D shortcut from the help panel

--- ui.js
+++ ui.js
@@ -2,8 +2,7 @@ function show_help() {
     document.getElementById("help-icon").src="assets/help.png";
     setTimeout(alert,200,
         `Keyboard shortcuts:
-            
-        Control-Alt-D: Show/Hide Settings button
+
         Control-Alt-C: Open Settings panel
         Control-H:     Open this help`
     );
