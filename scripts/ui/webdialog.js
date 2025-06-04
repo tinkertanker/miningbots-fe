3cacba8cd1b273940684866f54005f08b7488da0
danielh13210 <danielh13210@gmail.com>
2025-06-04 16:23:26 +0800

    Move stylesheet in JS to CSS file

--- scripts/ui/webdialog.js
+++ scripts/ui/webdialog.js
@@ -1,48 +1,24 @@
 function showDialog_(html,title,buttons){
     //create the dialog box
     const dialog = document.createElement('div');
-    dialog.style.position = 'fixed';
-    dialog.style.top = '50%';
-    dialog.style.left = '50%';
-    dialog.style.transform = 'translate(-50%, -50%)';
-    dialog.style.padding = '20px';
-    dialog.style.backgroundColor = 'white';
+    dialog.classList.add('dialog');
     dialog.classList.add("usedark");
-    dialog.style.border = '2px solid silver';
-    dialog.style.borderTop = '40px solid silver';
-    dialog.style.borderRadius="5px";
-    dialog.style.zIndex = '1000';
-    dialog.style.minWidth="200px";
 
     //place the text in the box
     dialog.innerHTML=html?html:"";
 
     //create the cover board to prevent clicking outside the winner box while it is open
     const coverBoard = document.createElement('div');
-    coverBoard.style.position='fixed';
-    coverBoard.style.top='0';
-    coverBoard.style.left='0';
-    coverBoard.style.backgroundColor='transparent';
-    coverBoard.style.width="100vw";
-    coverBoard.style.height="100vh";
-
+    coverBoard.classList.add('dialog-modal-coverboard');
 
     //create the close button
     const closeButton = document.createElement('button');
     closeButton.classList.add("nodark");
+    closeButton.classList.add("dialog-close");
     const x = document.createElement('p');
+    x.classList.add("dialog-xbutton");
     x.innerHTML="x";
-    x.style.top="-4px";
-    x.style.position="relative";
-    x.style.color='white';
     closeButton.appendChild(x);
-    closeButton.style.position = 'absolute';
-    closeButton.style.top = `-30px`;
-    closeButton.style.left = '3px';
-    closeButton.style.width=closeButton.style.height="20px";
-    closeButton.style.backgroundColor="red";
-    closeButton.style.borderRadius="50%";
-    closeButton.style.border="none";
     let overflow_prev=document.body.style.overflow;
     function close_dialog(){
         document.body.removeChild(dialog);
@@ -54,26 +30,19 @@ function showDialog_(html,title,buttons){
     //create the dialog title
     const dialogTitle=document.createElement("h3");
     dialogTitle.innerText=(typeof title == "string")?title:"[In-page dialog]";
-    dialogTitle.style.userSelect="none";
-    dialogTitle.style.position="absolute";
-    dialogTitle.style.top="-38px";
-    dialogTitle.style.left="50%";
-    dialogTitle.style.transform="translateX(-50%)";
+    dialogTitle.classList.add("dialog-title");
 
     //create the button box
     let buttonBox=document.createElement("div");
+    buttonBox.classList.add("dialog-buttonbox");
     buttonBox.classList.add("nodark");
-    buttonBox.style.display="flex";
-    buttonBox.style.justifyContent="center";
-    buttonBox.style.width="100%";
     if(typeof buttons!="object"){
         buttons=[{"text":"OK","action":"close_dialog"}];
     }
     buttons.forEach(button_descriptor => {
         let button=document.createElement("button");
+        button.classList.add("dialog-button");
         button.innerHTML=button_descriptor["text"];
-        button.style.marginLeft="3px";
-        button.style.marginRight="3px";
         let action=button_descriptor["action"];
         if (typeof action!="function"){
             action=()=>true;
