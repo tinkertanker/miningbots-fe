725aa1bc04e76a154bdb2acc83d18b2f8bb90f87
daniel <danielh13210@gmail.com>
2025-07-14 02:38:56 +0700

    Add a help icon in Settings

--- scripts/ui/help.js
+++ scripts/ui/help.js
@@ -7,3 +7,14 @@ function show_help() {
         ${primaryKey}-H:     Open this help`
     );
 }
+
+function show_settings_help() {
+    document.getElementById("help-icon").src="/images/ui/help.png";
+    setTimeout(alert,200,
+        `Keyboard shortcuts:
+
+        ${primaryKey}-O: Save settings, reload main page, and close this window
+        ${primaryKey}-C: Close this window without saving the settings
+        ${primaryKey}-A: Save settings and reload main page`
+    );
+}
