1ea7be5909ecf6b9e951ad685dd972dfe47943be
danielh13210 <danielh13210@gmail.com>
2025-07-26 14:55:52 +0800

    Fix exit codes

--- init.sh
+++ init.sh
@@ -18,10 +18,10 @@ function start_browser(){
     # update the ffconfig (Firefox Config) depending on the UI Mode
     if [ $UI_MODE == "debug" ]; then
       CLASS="Mining Bots (debug/test)"
-      ./utilities/update_ffconfig.py toolkit.legacyUserProfileCustomizations.stylesheets=false browser.tabs.inTitlebar=1 || exit
+      ./utilities/update_ffconfig.py toolkit.legacyUserProfileCustomizations.stylesheets=false browser.tabs.inTitlebar=1 || exit 1
     elif [ '(' "$UI_MODE" == "minimalist" ')' -o '(' "$UI_MODE" == "fullscreen" ')' ]; then
       CLASS="Mining Bots"
-      ./utilities/update_ffconfig.py toolkit.legacyUserProfileCustomizations.stylesheets=true browser.tabs.inTitlebar=0 || exit
+      ./utilities/update_ffconfig.py toolkit.legacyUserProfileCustomizations.stylesheets=true browser.tabs.inTitlebar=0 || exit 1
     else
       echo "Invalid UI mode" 1>&2
       return
