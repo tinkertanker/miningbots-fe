c93c73400a68ee145b0c88ec61b58f753ab8b749
danielh13210 <danielh13210@gmail.com>
2025-08-01 16:23:02 +0800

    Use proper way to manage menu

--- scripts/ui/toggle_navigation.js
+++ scripts/ui/toggle_navigation.js
@@ -1,37 +1,19 @@
 function toggleNavigation(){
   setTimeout(()=>{
     let link=document.getElementById('navbarDropdownMenuLink');
-    // toggle show
-    // HACK: add a second class to prevent bootstrap from tampering with our system
-    if (link.classList.contains('secret-show')){
-      link.classList.remove('show');
+    let dropdown=bootstrap.Dropdown.getOrCreateInstance(link);
+    if(link.classList.contains('secret-show')){
+      dropdown.hide();
       link.classList.remove('secret-show');
-    } else {
-      link.classList.add('show');
+    }else{
+      dropdown.show();
       link.classList.add('secret-show');
-    }
-    let dropdown=document.getElementById('dropdown-menu');
-    // toggle show
-    if (dropdown.classList.contains('show')){
-      dropdown.classList.remove('show');
-    } else {
-      dropdown.classList.add('show');
-    }
-    // toggle data-bs-popper
-    if (dropdown.getAttribute("data-bs-popper")){
-      dropdown.removeAttribute("data-bs-popper");
-    } else {
-      dropdown.setAttribute("data-bs-popper","static");
     }
   },0);
 }
 
 function showNavigation(){
   let link=document.getElementById('navbarDropdownMenuLink');
-  // add show
-  link.classList.add('show');
-  link.classList.add('secret-show');
-  let dropdown=document.getElementById('dropdown-menu');
-  dropdown.classList.add('show');
-  dropdown.setAttribute("data-bs-popper","static");
+  bootstrap.Dropdown.getOrCreateInstance(link).show();
 }
+ 
