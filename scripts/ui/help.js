c4acc4438e3fd67153681744526dcfaad66c8670
danielh13210 <danielh13210@gmail.com>
2025-08-04 13:55:51 +0800

    Add Export and Import to help.js

--- scripts/ui/help.js
+++ scripts/ui/help.js
@@ -14,6 +14,8 @@ function show_settings_help() {
         ${KeyboardUtilities.joinMnemonic('O')}: Save settings, reload main page, and close this window
         ${KeyboardUtilities.joinMnemonic('C')}: Close this window without saving the settings
         ${KeyboardUtilities.joinMnemonic('A')}: Save settings and reload main page
+        ${KeyboardUtilities.joinMnemonic('S')}: Export settings to settings.json
+        ${KeyboardUtilities.joinMnemonic('I')}: Load settings from a JSON file
         ${KeyboardUtilities.joinMnemonic('H')}: Show this help`
     );
 }
