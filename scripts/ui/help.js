55cf2bf0d26f6be80ad0271b1870a10f16c77c13
danielh13210 <danielh13210@gmail.com>
2025-08-09 09:55:41 +0800

    make order in help match order on html

--- scripts/ui/help.js
+++ scripts/ui/help.js
@@ -9,11 +9,11 @@ function show_help() {
 function show_settings_help() {
     if(isAnotherDialogShowing()) return; // Prevent showing another dialog if one is already open
     showDialog(
-        `<span style="white-space:nowrap;"><svgfile src="/images/ui/ok.svg"></svgfile> (OK): Save settings, reload main page, and close this window<br>
+        `<span style="white-space:nowrap;"><svgfile src="/images/ui/help.svg"></svgfile> (Help): Show this help<br>
+        <svgfile src="/images/ui/ok.svg"></svgfile> (OK): Save settings, reload main page, and close this window<br>
         <svgfile src="/images/ui/cancel.svg"></svgfile> (Cancel): Close this window without saving the settings<br>
         <svgfile src="/images/ui/apply.svg"></svgfile> (Apply): Save settings and reload main page<br>
         <svgfile src="/images/ui/export.svg"></svgfile> (Export): Export settings to settings.json<br>
-        <svgfile src="/images/ui/import.svg"></svgfile> (Import): Load settings from a JSON file<br>
-        <svgfile src="/images/ui/help.svg"></svgfile> (Help): Show this help<br></span>`
+        <svgfile src="/images/ui/import.svg"></svgfile> (Import): Load settings from a JSON file<br></span>`
     ,"Help");
 }
