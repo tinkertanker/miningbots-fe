1d09aa825c2ac2bf0218b3b3b82b853a1dab56c7
danielh13210 <danielh13210@gmail.com>
2025-08-11 16:07:57 +0800

    Revert "Fix alignment"
    This reverts commit 44692bdaa8c809f9cc4cbb55060f92eb4d3d1c7a.

--- scripts/ui/toggle_navigation.js
+++ scripts/ui/toggle_navigation.js
@@ -1,35 +1,14 @@
-let toggleNavigation, showNavigation;
-document.addEventListener('DOMContentLoaded', () => {
-  const navbarToggler = document.querySelector('.navbar-toggler');
+function toggleNavigation(event){
+  event.stopPropagation();
+  setTimeout(()=>{
+    let link=document.getElementById('navbarDropdownMenuLink');
+    let dropdown=bootstrap.Dropdown.getOrCreateInstance(link);
+    dropdown.toggle();
+  },0);
+}
 
-  const buttonDropdown = new bootstrap.Dropdown(navbarToggler, {
-    popperConfig: (defaultConfig) => ({
-      ...defaultConfig,
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: [0, 8],
-          },
-        },
-      ],
-    }),
-  });
-
-  const defaultDropdown = bootstrap.Dropdown.getOrCreateInstance(document.getElementById('navbarDropdownMenuLink'));
-  toggleNavigation = function(event) {
-    event.stopPropagation();
-
-    setTimeout(() => {
-      buttonDropdown.toggle();
-    }, 0);
-  }
-
-  showNavigation = function() {
-    if(window.getComputedStyle(navbarToggler).display === 'none') {
-      defaultDropdown.show();
-    } else {
-      buttonDropdown.show();
-    }
-  }
-});
+function showNavigation(){
+  let link=document.getElementById('navbarDropdownMenuLink');
+  bootstrap.Dropdown.getOrCreateInstance(link).show();
+}
+ 
