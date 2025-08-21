bdeb31410bb453f72c207bbffd2d0c62d2521c77
danielh13210 <danielh13210@gmail.com>
2025-08-21 14:24:54 +0800

    Clean double setDarkMode call

--- scripts/settings.js
+++ scripts/settings.js
@@ -380,7 +380,6 @@ function initialize_main(production_status) {
 function onChange(setting_update){
     if(setting_update.key=="theme"){
         setDarkMode(darkModeEnabled({"theme":setting_update.value}));
-        setDarkMode(darkModeEnabled({"theme":setting_update.value}));
     }
 
     update_reset_button(setting_update);
