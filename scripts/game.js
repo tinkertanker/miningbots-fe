cff3e15f8458a033b1d8af6e0466beb7d45fb9a9
danielh13210 <danielh13210@gmail.com>
2025-08-30 17:30:06 +0800

    Add comments

--- scripts/game.js
+++ scripts/game.js
@@ -360,8 +360,8 @@ function drawGame() {
             // console.log(response);
             // make the UI ready
             document.getElementById("bot-info-megacontainer").classList.remove("sidebar-hidden");
-
-            // return the games as a JS object
+            // ensure the response is OK
+            // if it is, return the games as a JS object
             if (response.ok) {
                 // console.log('games:', response);
                 LoadingBox.setStatus(LoadingBox.Status.LOADING_COMPLETED);
@@ -408,9 +408,11 @@ function drawGame() {
             }
         })
         // prep the map_config for the next function
+        // wait for it to return and decode it as json
         .then(async result => {
             let response = await result.response;
 
+            // also ensure the map_config response is OK
             if (response.ok) {
                 console.log('Map config fetch response:', response);
                 return { map_config: response.json(), game_id: result.game_id }; // pass down the game_id
@@ -419,6 +421,7 @@ function drawGame() {
             }
         })
         //result= Map config and game ID taken from server data
+        //this is where the actual rendering occurs
         .then(async result => {
             let map_config = await result.map_config;
             console.log('map_config:', map_config);
