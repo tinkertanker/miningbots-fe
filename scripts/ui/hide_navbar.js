14e230f0e9e214bee20875553cf9d501a09fde7d
danielh13210 <danielh13210@gmail.com>
2025-11-26 16:55:08 +0800

    Don't hide the navbar too early
    Otherwise, if the peek box is hovered at load it would add and remove the class (inefficient)

--- scripts/ui/hide_navbar.js
+++ scripts/ui/hide_navbar.js
@@ -10,8 +10,9 @@ document.addEventListener("DOMContentLoaded",(_e)=>{
       ModeManager.IS_PRODUCTION_MODE = true;
     setTimeout(() => {
       if (ModeManager.IS_PRODUCTION_MODE) {
-        let navbar=document.getElementById("navbar")
-        navbar.classList.add("production-hidden");
+        let navbar=document.getElementById("navbar");
+        if(!document.querySelector("#navbar-peek-triggerbox:hover"))
+          navbar.classList.add("production-hidden");
         let dropdown=document.getElementById("navbarDropdownMenuLink");
         dropdown.addEventListener("shown.bs.dropdown",()=>{
             navbar.classList.remove("production-hidden");
