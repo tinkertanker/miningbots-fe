6d1f7c29bda8b7f2f5491bda6ef69ee234740944
daniel <danielh13210@gmail.com>
2025-11-29 19:57:44 +0800

    Don't close navbar if tab focused
    This also allows clicking the logo hold the navbar open

--- scripts/ui/hide_navbar.js
+++ scripts/ui/hide_navbar.js
@@ -12,13 +12,6 @@ document.addEventListener("DOMContentLoaded",(_e)=>{
       if (ModeManager.IS_PRODUCTION_MODE) {
         let navbar=document.getElementById("navbar");
         navbar.classList.add("production-hidden");
-        let dropdown=document.getElementById("navbarDropdownMenuLink");
-        dropdown.addEventListener("shown.bs.dropdown",()=>{
-            navbar.classList.remove("production-hidden");
-        });
-        dropdown.addEventListener("hidden.bs.dropdown",()=>{
-            navbar.classList.add("production-hidden"); // put the class back
-        });
       }
     }, 400);
 });
